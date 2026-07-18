aa747ca3c51b9fad7cb5aedcb3b0f7b48851d5df
João Vitor <almeidaoliveira9697@gmail.com>
2026-07-18 15:37:56 -0300

    Implementa listagem de produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,6 +8,7 @@
  * @author Adm
  */
 
+
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -99,6 +100,33 @@ public class ProdutosDAO {
                 "Erro ao realizar venda: " + e.getMessage());
     }
 }
+ 
+   public ArrayList<ProdutosDTO> ListarProdutoVendido(){
+        String sql = "SELECT * FROM produtos WHERE status = Vendido";
         
+        conn = new conectaDAO().connectDB();
+        
+        try{
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while(resultset.next()){
+                
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao listar produtos: " + e.getMessage());
+        }
+        return listagem;
+   }
+    
 }
 
